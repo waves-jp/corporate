3b485e5339c62833fe592dce134cfee05cdb1b11
Ryotaro Hada <116790673+ryotarohada-waves@users.noreply.github.com>
2023-01-04 00:25:45 +0900

    update

--- src/pages/api/tokenuri/[v].js
+++ src/pages/api/tokenuri/[v].js
@@ -4,6 +4,7 @@ const TOKEN_URI = {
     'フリーランスWeb開発者の羽田と申します。屋号「WAVES」としてWebアプリケーションの開発スキルを提供しています。また、個人開発も行なっています。',
   image: '',
   site: 'https://waves-jp.com',
+  apiVersion: '1',
 }
 
 export default async function tokenUriV1(req, res) {
